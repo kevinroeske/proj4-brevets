bbead627cde955eb372aaf6a383e44da60fb14c1
Kevin Roeske <kroeske@localhost.localdomain>
2017-10-20 18:41:20 -0700

    comments in test suite

--- brevets/nosetests.py
+++ brevets/nosetests.py
@@ -1,4 +1,12 @@
 #test suite for brevet calculator
+#
+#Seven cases for each: an invalid (negative) distance, 0, a generic middle, and edge cases for the end.
+#Note that I've set my calculator to just return the generic start time if the distance is invalid
+#
+#This one may be incomplete, but it tests the 200km brevet distance extensively
+#
+#
+#
 import acp_times
 import arrow
 #from acp_times import open_time(control_dist_km, brevet_dist_km, brevet_start_time)
@@ -10,7 +18,7 @@ testFourHundred = [-5, 0, 100, 399, 400, 440, 441]
 testSixHundred = [-5, 0, 100, 599, 600, 660, 661]
 testThousand = [-5, 0, 100, 999, 1000, 11000, 1101]
 
-rsltTwo=['2017-01-01T00:00:00','2017-01-01T00:00','2017-01-01T02:56','2017-01-01T05:51','2017-01-01T05:52','2017-01-01T05:52','2017-01-01T00:00']
+rsltTwo=['2017-01-01T00:00','2017-01-01T00:00','2017-01-01T02:56','2017-01-01T05:51','2017-01-01T05:52','2017-01-01T05:52','2017-01-01T00:00']
 rsltThr=['2017-01-01T00:00','2017-01-01T02:56','2017-01-01T05:51','2017-01-01T00:00','2017-01-01T05:52','2017-01-01T05:52','2017-01-01T00:00']
 rsltFou=['2017-01-01T00:00','2017-01-01T02:56','2017-01-01T05:51','2017-01-01T00:00','2017-01-01T05:52','2017-01-01T05:52','2017-01-01T00:00']
 rsltSix=['2017-01-01T00:00','2017-01-01T02:56','2017-01-01T05:51','2017-01-01T00:00','2017-01-01T05:52','2017-01-01T05:52','2017-01-01T00:00']
