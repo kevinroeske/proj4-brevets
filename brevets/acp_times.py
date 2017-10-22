0ddf21c1dfa3c443f662fc844d0753a61d266f40
Kevin Roeske <kroeske@localhost.localdomain>
2017-10-22 11:48:39 -0700

    table-driven complete

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -19,8 +19,10 @@ import flask
 timezone_shift = 8      #this allows us to adjust the time shift due to arrow being timezone-aware. It reads everything as utc time but
                         #outputs based on local time zone. Huge pain, but this is a good workaround
 app = flask.Flask(__name__)
+
+leg_lengths = [200, 200, 200, 400]
 max_times = {200: 13.5, 300: 20, 400: 27, 600: 40, 1000: 75}
-min_speeds = [15, 11.428]
+min_speeds = [15, 15, 15, 11.428]
 max_speeds = [34, 32, 30, 28] 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -47,23 +49,15 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     if (control_dist_km >= brevet_dist_km and control_dist_km <= brevet_dist_km*1.1):
         control_dist_km = brevet_dist_km
     
-    if (control_dist_km >= 200):
-        leg_one_dist = 200
-    else:
-        leg_one_dist = control_dist_km
-
-    if (control_dist_km > 400):
-        leg_two_dist = 200
-    elif (control_dist_km > 200):
-        leg_two_dist = control_dist_km - 200
-
-    if (control_dist_km > 600):
-        leg_three_dist = 200
-        leg_four_dist = control_dist_km - 600
-    elif (control_dist_km > 400):
-        leg_three_dist = control_dist_km - 400
-    
-    hours_till_open = leg_one_dist/max_speeds[0] + leg_two_dist/max_speeds[1] + leg_three_dist/max_speeds[2] + leg_four_dist/max_speeds[3]
+    iterator = 0
+    total_distance = 0
+    for length in leg_lengths:
+        if (control_dist_km >= total_distance + length):
+            hours_till_open += length/max_speeds[iterator]
+        elif (control_dist_km > total_distance):
+            hours_till_open += (control_dist_km - total_distance)/max_speeds[iterator]
+        iterator += 1
+        total_distance += length
     
     opening_time =  arrow.get(brevet_start_time).shift(hours=+hours_till_open+timezone_shift)
     if (opening_time.second >= 30):
@@ -84,23 +78,25 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    hours_till_close = 0 
-    leg_one_dist = 0 
-    leg_two_dist = 0 
+    hours_till_close = 0
 
-    if (control_dist_km <= 0 or control_dist_km > brevet_dist_km*1.1): 
+    if (control_dist_km <= 0 or control_dist_km > brevet_dist_km*1.1):
         return arrow.get(brevet_start_time).shift(hours=+timezone_shift+1).isoformat()
 
     if (control_dist_km >= brevet_dist_km and control_dist_km <= brevet_dist_km*1.1):
         return arrow.get(brevet_start_time).shift(hours=+max_times[brevet_dist_km]+timezone_shift).isoformat()
-    
-    if (control_dist_km >= 600):
-        leg_one_dist = 600
-        leg_two_dist = control_dist_km - 600
-    else:
-        leg_one_dist = control_dist_km
 
-    hours_till_close = leg_one_dist/min_speeds[0] + leg_two_dist/min_speeds[1]
+    iterator = 0
+    total_distance = 0
+    for length in leg_lengths:
+        if (control_dist_km >= total_distance + length):
+            hours_till_close += length/min_speeds[iterator]
+        elif (control_dist_km > total_distance):
+            hours_till_close += (control_dist_km - total_distance)/min_speeds[iterator]
+        iterator += 1
+        total_distance += length
+
+
     closing_time =  arrow.get(brevet_start_time).shift(hours=+hours_till_close+timezone_shift)
     if (closing_time.second >= 30):
         closing_time = closing_time.shift(minutes=+1)
